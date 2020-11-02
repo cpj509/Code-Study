d13742c1a4c38ed7bedd4989fdd53597b4019112
CPJ <abzi110@naver.com>
2020-11-03 03:33:07 +0900

    simple url checker using go lang (topdown & use go routine)

---
+++ Go/src/github.com/cpj509/learngo/practice.goroutine.go
@@ -0,0 +1,37 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	// c := make(chan bool)
+// 	c := make(chan string)
+// 	people := []string{"nico", "flynn", "pj"}
+// 	for _, person := range people {
+// 		go isSexy(person, c)
+
+// 	}
+// 	for i := 0; i < len(people); i++ {
+// 		fmt.Println("waiting for", i)
+// 		fmt.Println(<-c)
+// 	}
+// 	// fmt.Println("waiting....")
+// 	// fmt.Println("Recieved this message:", <-c)
+// 	// fmt.Println("Recieved this message:", <-c)
+// }
+
+// // func sexyCount(person string) {
+// // 	for i := 0; i < 10; i++ {
+// // 		fmt.Println(person, "is sexy", i)
+// // 		time.Sleep(time.Second)
+// // 	}
+// // }
+
+// func isSexy(person string, c chan string) {
+// 	time.Sleep(time.Second * 2)
+// 	// fmt.Println(person)
+// 	// c <- true
+// 	c <- person + " is sexy"
+// }
---
+++ Go/src/github.com/cpj509/learngo/practice_URLChecker_topdown.go
@@ -0,0 +1,43 @@
+// //url checking topdown
+// package main
+
+// import (
+// 	"errors"
+// 	"fmt"
+// 	"net/http"
+// )
+
+// var errRequestFailed = errors.New("Request fail.")
+
+// func main() {
+//	var results = make(map[string]string)
+// 	// results := map[string]string{} //위 코드와 같음
+// 	urls := []string{
+// 		"https://www.naver.com/",
+// 		"https://www.daum.net/",
+// 		"https://www.google.co.uk",
+// 		"https://www.quora.com",
+// 	}
+// 	for _, url := range urls {
+// 		result := "OK"
+// 		err := hitURL(url)
+// 		if err != nil {
+// 			result = "FAILED"
+// 		}
+// 		results[url] = result
+// 	}
+// 	for url, result := range results {
+// 		fmt.Println(url, result)
+// 	}
+// }
+
+// func hitURL(url string) error {
+// 	fmt.Println("Checking:", url)
+// 	resp, err := http.Get(url)
+// 	if err != nil || resp.StatusCode >= 400 { //status code는 0부터 100, 200, 300까지는 rediraction을 해주고 400부터는 문제가 있다는 뜻이여서 이렇게 함.
+// 		fmt.Println(err, resp.StatusCode)
+// 		return errRequestFailed
+// 	}
+// 	return nil
+
+// }
