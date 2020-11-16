bfae7d1665ed0dfcb6c191239e65643f920c7177
CPJ <abzi110@naver.com>
2020-11-16 11:44:53 +0900

    study c++ (hackerrank)

---
+++ C++/hackerrank/30daysofcode/3.cpp
@@ -0,0 +1,27 @@
+//Day 3: Intro to Conditional Statements
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (N % 2 == 1)
+    {
+        cout << "Weird" << endl;
+    }
+    else if (N % 2 == 0)
+    {
+        if (N >= 2 && N <= 5 || N > 20)
+        {
+            cout << "Not Weird" << endl;
+        }
+        else
+        {
+            cout << "Weird" << endl;
+        }
+    }
+    return 0;
+}
