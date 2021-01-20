8478d58c3fc27890a95ae6082daead9da23ffa2a
DESKTOP-6QFIE2G\c <abzi110@naver.com>
2021-01-20 18:13:45 +0900

    Review java simple bank app and html study.

---
+++ Java/Basic/BankApp/src/bank/AppMain2.java
@@ -0,0 +1,139 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AppMain2 {
+    private static final ArrayList<Account> accountArrayList = new ArrayList<>();
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean run = true;
+        while(run){
+            System.out.println("---------------------------------------");
+            System.out.println("1. 계좌 생성 | 2. 계좌 목록 " +
+                    "| 3. 입금 | 4. 출금 | 5. 종료");
+            System.out.println("---------------------------------------");
+            System.out.print(">");
+
+            int selNum = sc.nextInt();
+            switch (selNum){
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    accountList();
+                    break;
+                case 3:
+                    deposit();
+                    break;
+                case 4:
+                    withdraw();
+                    break;
+                case 5:
+                    run = false;
+                    break;
+                default:
+                    System.out.println("올바른 번호를 입력해 주세요.");
+            }
+        }
+        System.out.println("프로그램 종료");
+        sc.close();
+    }
+
+    private static void withdraw() {
+        System.out.println("----------");
+        System.out.println("출금");
+        System.out.println("----------");
+        System.out.print("계좌번호 : ");
+        String accountNum = sc.next();
+        System.out.print("출금액 : ");
+        int money = sc.nextInt();
+
+        try {
+            Account account = findAccount(accountNum);
+            assert account != null;
+            if(account.getBalance() - money < 0){
+                System.out.println("잔액이 부족합니다.");
+                System.out.println("처리 결과 : 출금에 실패하였습니다.");
+                return;
+            }
+            account.setBalance(account.getBalance() - money);
+            System.out.println("처리 결과 : 출금에 성공하였습니다.");
+        } catch (Exception e) {
+            System.out.println("계좌를 찾을 수 없습니다.");
+            System.out.println("처리 결과 : 출금에 실패하였습니다.");
+        }
+    }
+
+    private static void deposit() {
+        System.out.println("----------");
+        System.out.println("입금");
+        System.out.println("----------");
+        System.out.print("계좌번호 : ");
+        String accountNum = sc.next();
+        System.out.print("입금액 : ");
+        int money = sc.nextInt();
+
+        try {
+            Account account = findAccount(accountNum);
+            assert account != null;
+            account.setBalance(account.getBalance() + money);
+            System.out.println("처리 결과 : 입금에 성공하였습니다.");
+        } catch (NullPointerException e) {
+            System.out.println("계좌를 찾을 수 없습니다.");
+            System.out.println("처리 결과 : 입금에 실패하였습니다.");
+        }
+    }
+
+    private static Account findAccount(String accountNum) {
+        Account account = null;
+        for (Account value : accountArrayList) {
+            if (value.getAccountNum().equals(accountNum)) {//외부 입력계좌와 일치하면
+                account = value;
+                return account;
+            }
+        }
+        return null;
+    }
+
+    private static void accountList() {
+        System.out.println("----------");
+        System.out.println("계좌 목록");
+        System.out.println("----------");
+        for (Account account : accountArrayList) {
+            account.showInfo();
+        }
+    }
+
+    private static void createAccount() {
+        System.out.println("----------");
+        System.out.println("계좌 생성");
+        System.out.println("----------");
+        String accountNum = "";
+        Loop1:
+        while(true) {
+            System.out.print("계좌 번호 : ");
+            accountNum = sc.next();//nextLine은 라인이 넘어감
+            for (Account account :
+                    accountArrayList) {
+                if (account.getAccountNum().equals(accountNum)) {
+                    System.out.println("동일한 계좌 번호가 존재합니다.");
+                    System.out.println("다시 입력 해 주세요.");
+                    continue Loop1;
+                }
+            }
+            break;
+        }
+
+        System.out.print("계좌주 : ");
+        String owner = sc.next();
+
+        System.out.print("초기 입금액 : ");
+        int balance = sc.nextInt();
+
+        Account newAccount = new Account(accountNum, owner, balance);
+        accountArrayList.add(newAccount);
+        System.out.println("처리 결과 : 정상 처리 되었습니다.");
+    }
+}
