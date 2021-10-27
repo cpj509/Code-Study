146bd2883fcffb8db0d637fd2fa84ec7ae1684ae
DESKTOP-3H80BVO\USER <abzi110@naver.com>
2021-10-27 12:51:47 +0900

    JS 강의 진행중. 이벤트. 이벤트에서 localstorage 사용해봄.

--- JS/Practice/app.js
+++ JS/Practice/app.js
@@ -3,18 +3,56 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const link = document.querySelector("a");
 
-function chkEvent(e){
-    e.preventDefault();
-    const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    
-    //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+//1. localstorage를 1번만 paintGreeting에서 조회하는 방식. 오브젝트를 파라미터로 받음.
+function chkEvent(e) {
+  e.preventDefault();
+  const typedUsername = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedUsername);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  paintGreeting();
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY); //local storage에서 값 조회.
 
-loginForm.addEventListener("submit", chkEvent);
+function paintGreeting() {
+  const username = localStorage.getItem("username");
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if (localStorage.getItem(USERNAME_KEY) === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", chkEvent);
+} else {
+  paintGreeting();
+}
+
+//2. localstorage를 2번 조회하지만 paintGreeting의 파라미터가 없는 방식.
+// function chkEvent(e) {
+//     e.preventDefault();
+//     const typedUsername = loginInput.value;
+//     localStorage.setItem(USERNAME_KEY, typedUsername);
+//     loginForm.classList.add(HIDDEN_CLASSNAME);
+
+//     paintGreeting();
+//   }
+
+//   const savedUsername = localStorage.getItem(USERNAME_KEY); //local storage에서 값 조회.
+
+//   function paintGreeting() {
+//     const username = localStorage.getItem("username");
+//     greeting.innerText = `Hello ${username}`;
+//     greeting.classList.remove(HIDDEN_CLASSNAME);
+//   }
+
+//   if (localStorage.getItem(USERNAME_KEY) === null) {
+//     loginForm.classList.remove(HIDDEN_CLASSNAME);
+//     loginForm.addEventListener("submit", chkEvent);
+//   } else {
+//     paintGreeting();
+//   }
